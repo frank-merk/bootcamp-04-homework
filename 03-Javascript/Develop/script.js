f31e41cf868bc1324e9cb4e77388d7a3303b99a3
Frank Merchlewitz <jlimhb@gmail.com>
2021-01-04 21:42:21 -0600

    Testing the confirmation to set up conditions for what content the password contains

--- 03-Javascript/Develop/script.js
+++ 03-Javascript/Develop/script.js
@@ -8,56 +8,60 @@ var numeral = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+// Go through the alphabet array and generate a new array of all capital letters
 var alphabetCaps = alphabet.map(alphabet => alphabet.toUpperCase());
+
+// Object that holds results of the randomizing functions to get a bunch of different random characters
+var random = {
+  special: getRandomChar,
+  number: getRandomNumber,
+  lower: getRandomLower,
+  upper: getRandomUpper
+};
 
 function getRandomChar() {
   return specialChar[Math.floor(Math.random() * specialChar.length)];
 }
 
-console.log(getRandomChar());
-
 function getRandomNumber(){
   return numeral[Math.floor(Math.random() * numeral.length)];
 }
-
-console.log(getRandomNumber());
 
 function getRandomLower(){
   return alphabet[Math.floor(Math.random() * alphabet.length)];
 }
 
-console.log(getRandomLower());
-
 function getRandomUpper(){
+  
   return alphabetCaps[Math.floor(Math.random() * alphabetCaps.length)];
 }
-console.log(getRandomUpper());
-// Go through the alphabet array and generate a new array of all capital letters
-
-
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  
+  var pwLength = parseInt(prompt("How long would you like your password to be? You can choose between 8 and 128"));
+  if (!pwLength || pwLength < 8 || pwLength > 128) {
+    alert("Invalid input, try again");
+  }
+  else {
+    var useSpecial = confirm("Would you like your password to contain special characters?");
+    var useNumber = confirm("Would you like your password to contain numbers?");
+    var useLower = confirm("Would you like your password to contain lower case letters?");
+    var useUpper = confirm("Would you like your password to contain upper case letters?");
+    console.log(useSpecial, useNumber, useLower, useUpper);
+  }
+
+
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  //var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  //passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-/* function generatePassword() {
-  var length = 8,
-      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-      retVal = "";
-  for (var i = 0, n = charset.length; i < length; ++i) {
-      retVal += charset.charAt(Math.floor(Math.random() * n));
-  }
-  return retVal;
-} */
-
